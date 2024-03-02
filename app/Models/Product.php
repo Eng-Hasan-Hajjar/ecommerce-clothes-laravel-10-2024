b7d61e5747429f35b5e20192c4a0303b3c858ac8
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-03-02 09:58:13 -0800

    create categor model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+        // اسم الجدول في قاعدة البيانات
+        protected $table = 'products';
+
+        // الخصائص التي يمكن التعديل عليها
+        protected $fillable = ['name', 'description', 'size', 'color', 'price', 'category_id'];
+
+        // الخصائص التي يجب أن تكون محمية وغير قابلة للتعديل
+        protected $guarded = ['id', 'created_at', 'updated_at'];
+
+        // علاقة المنتج بفئته
+        public function category()
+        {
+            return $this->belongsTo(Category::class);
+        }
 }
