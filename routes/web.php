12aaba6aa36fdf7802df382548f3541a3417f699
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-03-02 10:39:49 -0800

    create model and controller for vendors

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('/', function () {
+    return view('frontend.home');
+});
+/*
 Route::get('/', function () {
     return view('welcome');
 });
-
+*/
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
