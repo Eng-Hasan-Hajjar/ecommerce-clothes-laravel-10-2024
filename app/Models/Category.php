d27c0f828072fb3e9046981cd58886f28659887e
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-03-02 10:00:59 -0800

    create front end folder and file

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+     // اسم الجدول في قاعدة البيانات
+     protected $table = 'categories';
+
+     // الخصائص التي يمكن التعديل عليها
+     protected $fillable = ['name', 'description'];
+
+     // الخصائص التي يجب أن تكون محمية وغير قابلة للتعديل
+     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+     // علاقة الفئة بالمنتجات
+     public function products()
+     {
+         return $this->hasMany(Product::class);
+     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -22,4 +22,5 @@ class Product extends Model
         {
             return $this->belongsTo(Category::class);
         }
+
 }
